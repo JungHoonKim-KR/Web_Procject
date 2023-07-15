6751c0b847c423b2b1bc282373a4aa2d41db3ac2
JungHoon Kim <kik1232198@gmail.com>
2023-07-15 23:02:15 +0900

    카테고리

---
+++ src/main/java/post/study/entity/Category.java
@@ -0,0 +1,21 @@
+package post.study.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter@Setter
+public class Category {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String category;
+
+
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+}
