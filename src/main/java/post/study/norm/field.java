733679ad6930fc324e8abdc3170dc778c723ff2c
JungHoon Kim <kik1232198@gmail.com>
2023-07-22 23:21:35 +0900

    분야, 언어 정의

---
+++ src/main/java/post/study/norm/field.java
@@ -0,0 +1,6 @@
+package post.study.norm;
+
+public enum field {
+    프론트엔드, 백엔드
+
+}
---
+++ src/main/java/post/study/norm/language.java
@@ -0,0 +1,8 @@
+package post.study.norm;
+
+public enum language {
+    //언어
+    C,JAVA,PYTHON,CSS,JAVASCRIPT,typescript,SQL,PHP,KOTLIN,GO,SWIFT
+
+}
+
