8ca31145b87ae278e64b4d1748f64bb9d96c716e
JungHoon Kim <kik1232198@gmail.com>
2023-07-25 19:33:03 +0900

    언어, 분야 조회 추가

--- src/main/java/post/study/repository/FieldMemberRepository.java
+++ src/main/java/post/study/repository/FieldMemberRepository.java
@@ -1,8 +1,18 @@
 package post.study.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import post.study.entity.Field_Member;
+
+import javax.transaction.Transactional;
+
 @Repository
 public interface FieldMemberRepository extends JpaRepository<Field_Member,Long> {
+    @Modifying
+    @Transactional
+    @Query("delete from Field_Member f where f.member.id=:memberId")
+    void deleteFeild_MemberByMemberId(@Param("memberId")Long memberId);
 }
--- src/main/java/post/study/repository/LanguageMemberRepository.java
+++ src/main/java/post/study/repository/LanguageMemberRepository.java
@@ -1,11 +1,20 @@
 package post.study.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import post.study.entity.Language_Member;
-@Repository
-public interface LanguageMemberRepository extends JpaRepository<Language_Member,Long> {
 
+import javax.transaction.Transactional;
+
+@Repository
+public interface LanguageMemberRepository extends JpaRepository<Language_Member, Long> {
+    @Modifying
+    @Transactional
+    @Query("delete from Language_Member l where l.member.id=:memberId")
+    void deleteLanguage_MemberByMemberId(@Param("memberId") Long memberId);
 }
 
 
