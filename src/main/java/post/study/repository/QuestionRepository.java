a242d65b3859b42c130ebcf6e0643e1056162935
JungHoon Kim <kik1232198@gmail.com>
2023-07-27 20:51:19 +0900

    .

--- src/main/java/post/study/repository/QuestionRepository.java
+++ src/main/java/post/study/repository/QuestionRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import post.study.entity.Project;
 import post.study.entity.Question;
 
 @Repository
