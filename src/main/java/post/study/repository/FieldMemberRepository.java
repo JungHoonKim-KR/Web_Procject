4e9940a935c3b62087ba208af8dd031ac1655d9a
JungHoon Kim <kik1232198@gmail.com>
2023-07-31 23:41:04 +0900

    .

--- src/main/java/post/study/repository/FieldMemberRepository.java
+++ src/main/java/post/study/repository/FieldMemberRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import post.study.entity.Field_Member;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface FieldMemberRepository extends JpaRepository<Field_Member,Long> {
