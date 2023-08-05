be8816183ce9dc24f401b43fda55159c3eaebee5
JungHoon Kim <kik1232198@gmail.com>
2023-08-05 21:45:37 +0900

    .

--- src/test/java/post/study/StudyApplicationTests.java
+++ src/test/java/post/study/StudyApplicationTests.java
@@ -1,13 +1,12 @@
 package post.study;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @SpringBootTest
 class StudyApplicationTests {
 
-	@Test
-	void contextLoads() {
+
 	}
 
-}
