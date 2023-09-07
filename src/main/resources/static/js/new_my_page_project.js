6e9e9c21407f69c930d6e808eaa0002adc31bb20
JungHoon Kim <kik1232198@gmail.com>
2023-09-07 22:17:47 +0900

    마이페이지 프로필 업데이트

---
+++ src/main/resources/static/js/new_my_page_project.js
@@ -0,0 +1,9 @@
+
+//프로젝트가 몇개인지
+var counting=document.querySelector(".counting");
+
+//프로젝트 개수를 누르면 프로젝트 관리페이지로 이동
+counting.addEventListener("click",function(){
+    location.href='new_my_page_project.html';
+});
+
