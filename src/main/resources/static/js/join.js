33c415434885dffe65bcb9c505e4c40bbbe4645a
JungHoon Kim <kik1232198@gmail.com>
2023-09-13 00:02:19 +0900

    회원가입 정규표현식 추가

---
+++ src/main/resources/static/js/join.js
@@ -0,0 +1,45 @@
+function goform() {
+    let userName = document.querySelector("#username").value;
+    let email = document.querySelector("#emailId").value;
+    let password = document.querySelector("#password").value;
+    let phoneNumber = document.querySelector("#phoneNumber").value;
+
+    let regEmailId = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+    let regPassword = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/i;
+    let regPhoneNumber =/^01([0|1|6|7|8|9])-?([0-9]{4})-?([0-9]{4})$/;
+
+
+    if (userName == "") {
+        alert("이름을 입력해주세요");
+        return false;
+
+    }//if userName reg
+
+    if (email == "") {
+        alert("이메일을 입력해주세요.");
+        return false;
+
+    } else if (!regEmailId.test(email)) {
+        alert("이메일 형식이 올바르지 않습니다.");
+        return false;
+    } //if email reg
+
+    if (password == "") {
+        alert("비밀번호를 입력해주세요.");
+        return false;
+    } else if (!regPassword.test(password)) {
+        alert("비밀번호 형식이 올바르지 않습니다.");
+        return false;
+    }
+
+    if (phoneNumber == "") {
+        alert("전화번호를 입력해주세요.");
+        return false;
+    }else if(!regPhoneNumber.test(phoneNumber)){
+        alert("전화번호 형식이 올바르지 않습니다.");
+        return false;
+    }
+
+   document.querySelector("#form").submit();
+
+}
