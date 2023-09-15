702326cc59decd37d1e8f51049b01340053fd4ee
JungHoon Kim <kik1232198@gmail.com>
2023-09-15 22:29:22 +0900

    마이페이지 배너 추가

--- src/main/resources/static/js/new_my_page.js
+++ src/main/resources/static/js/new_my_page.js
@@ -10,12 +10,14 @@ var edit=document.querySelector(".edit");
 var inform_container=document.querySelector(".inform_container");
 //수정하기를 눌렀을때, 뜰 수정용 개인정보
 var edit_inform_container=document.querySelector(".edit_inform_container");
+var changing_profile=document.querySelector(".changing_profile");
 
 edit_inform_container.style.display="none";
 //수정하기를 누르면 원래개인정보는 안보이고 수정용 개인정보를 띄움
 edit.addEventListener("click",function(){
     inform_container.style.display="none";
     edit_inform_container.style.display="block";
+    changing_profile.style.visibility="visible";
 });
 
 
@@ -25,6 +27,7 @@ var edit_cancle=document.querySelector(".edit_cancle");
 edit_cancle.addEventListener("click",function(){
     inform_container.style.display="block";
     edit_inform_container.style.display="none";
+    changing_profile.style.visibility="visible";
 });
 
 //계정탈퇴
@@ -33,3 +36,11 @@ remove_account.addEventListener("click",function(){
     location.href='remove_account.html';
 });
 
+var donga=document.querySelector('.donga');
+donga.addEventListener('mouseover',function(){
+    donga.style.cursor= "pointer";
+})
+
+donga.addEventListener('click',function(){
+    location.href='main_post.html';
+})
