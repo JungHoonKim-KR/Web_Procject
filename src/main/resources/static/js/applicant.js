fed2019d09169706078ad9b1fe921560499756f9
JungHoon Kim <kik1232198@gmail.com>
2023-09-26 21:05:43 +0900

    .

--- src/main/resources/static/js/applicant.js
+++ src/main/resources/static/js/applicant.js
@@ -1,12 +1,13 @@
-var join_btn=document.querySelectorAll(".join_btn");
-var userEmail=document.querySelector("#userEmail").value;
+var approve_btn=document.querySelectorAll(".approve_btn");
+var refuse_btn=document.querySelectorAll(".refuse_btn");
+var userEmail=document.querySelectorAll("#userEmail");
 var projectId=document.querySelector("#projectId").value;
-join_btn.forEach(function (element){
-    element.addEventListener("click",function (){
+approve_btn.forEach(function (item,index){
+    item.addEventListener("click",function (){
         var url = "/mypage-project/approve";
         var param1 ="value="+encodeURIComponent(this.value);
         var param2="id="+encodeURIComponent(projectId);
-        var param3="emailId="+encodeURIComponent(userEmail);
+        var param3="emailId="+encodeURIComponent(userEmail[index].value);
 
         fetch(url + '?' + param1+"&"+param2+"&"+param3, {
             method: "GET",
@@ -15,13 +16,46 @@ join_btn.forEach(function (element){
             },
         })
             .then(response => response.text())
-            .catch(error => {
-                // Handle the error case here if needed
+            .then(data=> {
+                if (data == "승인")
+                    alert("승인하였습니다.");
+                else{
+                    alert("오류")
+                }
+
             });
-        if(this.value=="승인")
-            alert("승인하였습니다.")
-        else if(this.value="거절")
-            alert("거절하였습니다.")
+
+        setTimeout(function(){
+            location.reload();
+        }, 300);
+
+    })
+
+})
+
+refuse_btn.forEach(function (item,index){
+    item.addEventListener("click",function (){
+        var url = "/mypage-project/approve";
+        var param1 ="value="+encodeURIComponent(this.value);
+        var param2="id="+encodeURIComponent(projectId);
+        var param3="emailId="+encodeURIComponent(userEmail[index].value);
+
+        fetch(url + '?' + param1+"&"+param2+"&"+param3, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+            .then(response => response.text())
+            .then(data=> {
+                if (data == "거절")
+                    alert("거절하였습니다.");
+                else{
+                    alert("오류")
+                }
+
+            });
+
         setTimeout(function(){
             location.reload();
         }, 300);
